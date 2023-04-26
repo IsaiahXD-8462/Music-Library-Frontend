7ccee25b2253c5185511c3534510d49c16f10a08
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-04-26 16:18:12 -0400

    changed function

--- music_library/src/index.js
+++ music_library/src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOMroot.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+        <App />
+  </React.StrictMode>
+);
 
