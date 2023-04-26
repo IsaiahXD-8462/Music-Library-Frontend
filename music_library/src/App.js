5417e0e388380dc15cecb920da1db7e7e60a940b
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-04-26 01:37:24 -0400

    Created Table displaying song entries

--- music_library/src/App.js
+++ music_library/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import SongDisplayTable from './Components/Song Display Table/SongDisplayTable';
+
 
 function App() {
+
+  const [songs, setSongs] = useState([{song: '', artist: '', album: '', release_date: '', genre: '', likes: 0}])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SongDisplayTable parentSongs={songs} />
     </div>
   );
 }
---
+++ music_library/src/Components/Song Display Table/SongDisplayTable.jsx
@@ -0,0 +1,35 @@
+
+const SongDisplayTable = (props) => {
+    return ( 
+        <table>
+        <thead>
+          <tr>
+            <th>Entry Number</th>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Release Date</th>
+            <th>Genre</th>
+            <th>Likes</th>
+          </tr>
+        </thead>
+        <tbody>
+            {props.parentSongs.map((entry, index) => {
+            return (
+                <tr>
+                    <td>{index + 1}</td> 
+                    <td>{entry.song}</td>
+                    <td>{entry.artist}</td>
+                    <td>{entry.album}</td>
+                    <td>{entry.release_date}</td>
+                    <td>{entry.genre}</td>
+                    <td>{entry.likes}</td>
+                </tr>
+            );
+          })}
+        </tbody>
+      </table>
+     );
+}
+ 
+export default SongDisplayTable;
